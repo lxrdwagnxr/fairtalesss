ef87ed06b505a23aa5516c42f260bebdf9909585
lxrdwagnxr <radbor432@mail.ru>
2025-01-18 00:34:16 +0600

    whx?
    added rxutes and 2 new fairtales
    "Линялый фосфор"
    "Прокашивать монументальный"

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Header from "./Header";
 import FairTaleList from "./FairTaleList";
-export default function App() {
-  return (
-      <main>
-        <Header />
-        <FairTaleList />
-      </main>
-  );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FairyTaleStory from './FairyTaleStory'; // Добавили импорт компонента FairyTaleStory
+
+function App() {
+    return (
+        <Router>
+            <main>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<FairTaleList />} />
+                    <Route path="/story/:taleId" element={<FairyTaleStory />} />
+                </Routes>
+            </main>
+        </Router>
+    );
 }
+
+export default App;
--- FairTaleCard.jsx
+++ FairTaleCard.jsx
@@ -2,19 +2,21 @@
 
 // FairyTaleCard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const FairyTaleCard = ({ fairyTale }) => { // Исправлено
-  return (
-    <div className="fairytale-card">
-      <img src={fairyTale.image} alt={fairyTale.name} className="fairytale" />
-      <h2 className="fairytale-name">{fairyTale.name}</h2>
-      <p className="fairytale-description">{fairyTale.description}</p>
-      <p className="fairytale-price">{fairyTale.price}</p>
-      <a href={fairyTale.link} target="_blank" rel="noopener noreferrer">
-        <button className="fairytale-button">Перейти к сказке</button>
-      </a>
-    
-    </div>
-  );
+const FairyTaleCard = ({ fairyTale }) => {
+    return (
+        <div className="fairytale-card">
+            <Link to={`/story/${fairyTale.id}`}>
+                <img src={fairyTale.image} alt={fairyTale.name} className="fairytale" />
+                <h2 className="fairytale-name">{fairyTale.name}</h2>
+                <p className="fairytale-description">{fairyTale.description}</p>
+                <p className="fairytale-price">{fairyTale.price}</p>
+                <button className="fairytale-button">Перейти к сказке</button>
+            </Link>
+        </div>
+    );
 };
+
+
 export default FairyTaleCard; 
